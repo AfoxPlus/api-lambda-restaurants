95510a97841d00388e43d4419ca5ab66f72d83d3
Valentin Mendoza Torres <jmendozat13@gmail.com>
2023-06-05 22:25:30 -0500

    Find by code lambda implemented (#3)

--- src/core/repositories/database/MongoDBRestaurantRepository.ts
+++ src/core/repositories/database/MongoDBRestaurantRepository.ts
@@ -4,30 +4,50 @@ import { RestaurantDocument, RestaurantModel } from "@core/repositories/database
 import { RegistrationStateModel } from "@core/repositories/database/models/registration.state.model";
 
 export class MongoDBRestaurantRepository implements RestaurantRepository {
+
+    findRestaurant = async (code: string): Promise<Restaurant> => {
+        try {
+            const restaunratDodocument: RestaurantDocument = await RestaurantModel.findById(code).
+                populate({ path: 'registrationState', model: RegistrationStateModel })
+            return this.documentToRestaurant(restaunratDodocument)
+
+        } catch (err) {
+            console.log(err)
+            throw new Error("Internal Error")
+        }
+    }
+
     fetchHome = async (): Promise<Restaurant[]> => {
         try {
-                const restaurantDocuments: RestaurantDocument[] = await RestaurantModel.find().
-                populate({ path: 'registrationState', model: RegistrationStateModel})
-                return this.documentsToRestaurant(restaurantDocuments)
-        }catch(err) {
+            const restaurantDocuments: RestaurantDocument[] = await RestaurantModel.find().
+                populate({ path: 'registrationState', model: RegistrationStateModel })
+            return this.documentsToRestaurant(restaurantDocuments)
+        } catch (err) {
             console.log(err)
             throw new Error("Internal Error")
         }
     }
 
     private documentsToRestaurant(documents: RestaurantDocument[]): Restaurant[] {
-        const restaurants: Restaurant[] = documents.map((document) => ({
+        const restaurants: Restaurant[] = documents.map((document) => (
+            this.documentToRestaurant(document)
+        ))
+        return restaurants
+    }
+
+    private documentToRestaurant(document: RestaurantDocument): Restaurant {
+        const restaurants: Restaurant = {
             code: document._id.toString(),
             name: document.name,
             description: document.description,
             phone: document.phone,
-            email: document.email ,
+            email: document.email,
             urlImageLogo: document.urlImageLogo,
-            registrationState: {  
+            registrationState: {
                 code: document.registrationState.code,
                 state: document.registrationState.state
             }
-        }))
+        }
         return restaurants
     }
 
