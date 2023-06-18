d9f7db757542457ecd33a0c0edc6302f9772cc78
Valentin Mendoza <jmendozat13@gmail.com>
2023-06-18 18:57:48 -0500

    Added lambda to create restaurant

--- src/core/entities/RegistrationState.ts
+++ src/core/entities/RegistrationState.ts
@@ -1,4 +1,5 @@
 export interface RegistrationState {
-    code: string,
-    state: string
+    id?: string,
+    code?: string,
+    state?: string
 }
--- src/core/repositories/RestaurantRepository.ts
+++ src/core/repositories/RestaurantRepository.ts
@@ -3,4 +3,5 @@ import { Restaurant } from "@core/entities/Restaurant";
 export interface RestaurantRepository {
     fetchHome(): Promise<Restaurant[]>
     findRestaurant(code: string): Promise<Restaurant>
+    add(restaurant: Restaurant)
 }
