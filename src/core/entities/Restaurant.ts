2dc7a23141a3f389d67670521b5f2ad8aa86e9f1
Valentin Mendoza <jmendozat13@gmail.com>
2023-06-20 00:36:21 -0500

    Added auth method

--- src/core/entities/Restaurant.ts
+++ src/core/entities/Restaurant.ts
@@ -5,14 +5,14 @@ export interface Restaurant {
     code?: string,
     key: string,
     name: string,
-    description: string,
-    phone: string,
+    description?: string,
+    phone?: string,
     email?: string,
-    address: string,
+    address?: string,
     urlImageLogo: string,
-    ownDelivery: Boolean,
-    paymentMethods: [{ paymentMethod: string }],
-    attentionSchedule: string,
-    subscription: Subscription,
-    registrationState: RegistrationState
+    ownDelivery?: Boolean,
+    paymentMethods?: [{ paymentMethod: string }],
+    attentionSchedule?: string,
+    subscription?: Subscription,
+    registrationState?: RegistrationState
 }
